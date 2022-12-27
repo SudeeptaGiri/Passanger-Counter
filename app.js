a57be592e8d888d3b33bd2e766a4ac3e9386c433
Sudeepta Giri <66957676+SudeeptaGiri@users.noreply.github.com>
2022-12-27 19:42:38 +0530

    upload
    files are uploaded

---
+++ app.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
+function increment() {
+	count = count + 1;
+	countEl.innerText = count;
+}
+let saveEl = document.getElementById("save-el");
+
+function save() {
+	saveEl.textContent += count + " - ";
+	count = 0;
+	countEl.innerText = 0;
+}
